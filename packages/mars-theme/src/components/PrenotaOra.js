2308a4e560e91f943892e0318fda9af8fb75b155
Maxime Di Chiara <maxime.dichiara@spindox.it>
2022-03-03 11:07:44 +0100

    fix prenota ora button

--- packages/mars-theme/src/components/PrenotaOra.js
+++ packages/mars-theme/src/components/PrenotaOra.js
@@ -5,7 +5,7 @@ import translations from "../translations";
 
 const PrenotaOra = ({actions, state, size, variant = 'contained', color = 'primary', ...props}) => {
     return (
-        <Button {...props} onClick={() => window.open('https://wp.hesperia.it/wp-content/uploads/2021/03/modalita%CC%80-prenotazione.pdf','_blank')/*actions.theme.toggleTuoTempo('')*/} size={size} variant={variant} color={color}>{translations(state.theme.lang, 'prenotaOra')}</Button>
+        <Button {...props} onClick={() => actions.router.set(state.theme.urlPrefix+'/prenota')} size={size} variant={variant} color={color}>{translations(state.theme.lang, 'prenotaOra')}</Button>
     )
 }
 
